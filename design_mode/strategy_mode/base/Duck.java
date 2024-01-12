462c847a86172928d5650be959c114b0aa1832b1
ch <956718871@qq.com>
2024-01-12 20:03:18 +0800

    使用继承实现

---
+++ design_mode/strategy_mode/base/Duck.java
@@ -0,0 +1,16 @@
+package design_mode.strategy_mode.base;
+
+public class Duck {
+    public void quack(){
+        System.out.println("gua gua");
+    };
+    public void swim(){
+        System.out.println("swim");
+    };
+    public void fly(){
+        System.out.println("fly");
+    };
+    public void display(){
+        System.out.println("duck");
+    }
+}
