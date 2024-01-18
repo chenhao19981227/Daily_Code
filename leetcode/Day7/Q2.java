9fd76f27bf0ef602f63631fa9fd1f0f5449889a2
ch <956718871@qq.com>
2024-01-18 11:33:49 +0800

    11 盛水最多的容器 双指针 贪心

---
+++ leetcode/Day7/Q2.java
@@ -0,0 +1,17 @@
+package leetcode.Day7;
+
+public class Q2 {
+    public int maxArea(int[] height) {
+        int left=0,right=height.length-1;
+        int res=0;
+        while (left<right){
+            res=Math.max(res,Math.min(height[left],height[right])*(right-left));
+            if(height[left]<=height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return res;
+    }
+}
