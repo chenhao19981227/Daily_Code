5b44d25fefa320d086ff7f7730e3049d4c3c5643
ch <956718871@qq.com>
2024-01-19 11:34:18 +0800

    pizza店的普通实现

---
+++ design_mode/factory_mode/example/CheesePizza.java
@@ -0,0 +1,23 @@
+package design_mode.factory_mode.example;
+
+public class CheesePizza extends Pizza{
+    @Override
+    public void prepare() {
+
+    }
+
+    @Override
+    public void bake() {
+
+    }
+
+    @Override
+    public void cut() {
+
+    }
+
+    @Override
+    public void box() {
+
+    }
+}
---
+++ design_mode/factory_mode/example/GreekPizza.java
@@ -0,0 +1,24 @@
+package design_mode.factory_mode.example;
+
+public class GreekPizza extends Pizza{
+
+    @Override
+    public void prepare() {
+
+    }
+
+    @Override
+    public void bake() {
+
+    }
+
+    @Override
+    public void cut() {
+
+    }
+
+    @Override
+    public void box() {
+
+    }
+}
