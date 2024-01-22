23b9878acc0d345687028a4938b83d69adf3768e
ch <956718871@qq.com>
2024-01-22 17:06:20 +0800

    多线程场景下单例模式的实现

--- design_mode/singleton_mode/Singleton2.java
+++ design_mode/singleton_mode/Singleton2.java
@@ -2,11 +2,11 @@ package design_mode.singleton_mode;
 
 public class Singleton2 {
     // 多线程场景下单例模式的实现
-    private static Singleton1 singleton1;
-    public static synchronized Singleton1 getInstance(){
-        if(singleton1==null){
-            singleton1=new Singleton1();
+    private static Singleton2 singleton2;
+    public static synchronized Singleton2 getInstance(){
+        if(singleton2==null){
+            singleton2=new Singleton2();
         }
-        return singleton1;
+        return singleton2;
     }
 }
