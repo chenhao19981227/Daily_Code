072af0949390b2cfc9b0ea597feaced59fdec99f
chenhao19981227 <96047193+chenhao19981227@users.noreply.github.com>
2024-01-25 13:27:10 +0800

    重构6.5 改变函数声明 简单做法

---
+++ reconstruction/chapter6/rename_function/RenameFunction.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+class RenameFunction{
+  public double circum(double radius){
+    return 2*Math.PI*radius;
+  }
+  // 简单做法
+  public double circumference(double radius){
+      return 2*Math.PI*radius;
+  }
+}
