365162cb10bcf78457079b6a340adf2ab627f7a9
chenhao19981227 <96047193+chenhao19981227@users.noreply.github.com>
2024-01-25 11:52:13 +0800

    735 小行星碰撞 栈 数组 模拟

---
+++ leetcode/D14/Q2.java
@@ -0,0 +1,33 @@
+class Q2 {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> stack=new ArrayDeque<>();
+        boolean flag=true;
+        for(int i=0;i<asteroids.length;i++){
+            int plain=asteroids[i];
+            if(plain<0){
+                while(!stack.isEmpty()&&stack.peekFirst()>0){
+                    if(stack.peekFirst()+plain<0){
+                        stack.pollFirst();
+                    }else if(stack.peekFirst()+plain==0){
+                        stack.pollFirst();
+                        flag=false;
+                        break;
+                    }else{
+                        flag=false;
+                        break;
+                    }
+                }
+                if(flag)stack.addFirst(plain);
+                flag=true;
+            }else{
+                stack.addFirst(plain);
+            }
+        }
+        int len=stack.size();
+        int[] res=new int[len];
+        for(int i=0;i<len;i++){
+            res[i]=stack.pollLast();
+        }
+        return res;
+    }
+}
