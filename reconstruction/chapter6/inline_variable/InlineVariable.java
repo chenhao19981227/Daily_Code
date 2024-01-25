4a6c31e8a7ed68e5fbd5d3eac69c66c734629c4d
chenhao19981227 <96047193+chenhao19981227@users.noreply.github.com>
2024-01-25 12:35:51 +0800

    重构6.4 内联函数 示例+内联后

---
+++ reconstruction/chapter6/inline_variable/InlineVariable.java
@@ -0,0 +1,10 @@
+class InlineVariable{
+  // 去除没必要的变量——如果式子本身已经足够清晰
+  public int inlineVariable(){
+    int result=Integer.MAX_VALUE;
+    return result;
+  }
+  public int afterInlineVariable(){
+    return Integer.MAX_VALUE;
+  }
+}
