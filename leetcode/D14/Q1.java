f9174ac331a4d0a888ed08d79faef19a8175cf14
chenhao19981227 <96047193+chenhao19981227@users.noreply.github.com>
2024-01-25 11:14:06 +0800

    2390 从字符串中移除星号 栈 字符串

---
+++ leetcode/D14/Q1.java
@@ -0,0 +1,18 @@
+class Q1 {
+    public String removeStars(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!stack.isEmpty()&&c=='*'&&stack.peekFirst()!='*'){
+                stack.pollFirst();
+            }else{
+                stack.addFirst(c);
+            }
+        }
+        StringBuffer sb=new StringBuffer();
+        while(!stack.isEmpty()){
+            sb.append(stack.pollLast());
+        }
+        return sb.toString();
+    }
+}
