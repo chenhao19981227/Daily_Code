935a14395ff89d9fdeeb37996a94a773aa81d777
ch <956718871@qq.com>
2024-01-26 12:27:19 +0800

    重构6.7 变量改名

---
+++ reconstruction/chapter6/rename_variable/RenameVariable.java
@@ -0,0 +1,9 @@
+package reconstruction.chapter6.rename_variable;
+
+public class RenameVariable {
+    double height;
+    double width;
+    double a=height*width;
+    // rename
+    double area=height*width;
+}
