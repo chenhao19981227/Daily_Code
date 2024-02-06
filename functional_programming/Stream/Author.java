6f3ac60b4ac04804196482781f178d8d7bde9225
ch <956718871@qq.com>
2024-02-06 12:42:47 +0800

    Stream数据准备+入门案例

---
+++ functional_programming/Stream/Author.java
@@ -0,0 +1,25 @@
+package functional_programming.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode//重写equals和hashcode，用于后期的去重使用
+public class Author {
+    //id
+    private Long id;
+    //姓名
+    private String name;
+    //年龄
+    private Integer age;
+    //简介
+    private String intro;
+    //作品
+    private List<Book> books;
+}
