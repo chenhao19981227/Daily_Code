2498d39076741a1b861de0e337d328dac089ccd6
ch <956718871@qq.com>
2024-02-29 10:09:37 +0800

    面试题02.08 环路检测 链表 快慢指针

---
+++ leetcode/Day49/Q1.java
@@ -0,0 +1,29 @@
+package leetcode.Day49;
+
+import leetcode.ListNode;
+
+public class Q1 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            if (fast.next != null) {
+                fast = fast.next.next;
+            } else {
+                return null;
+            }
+            if (fast == slow) {
+                ListNode ptr = head;
+                while (ptr != slow) {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
