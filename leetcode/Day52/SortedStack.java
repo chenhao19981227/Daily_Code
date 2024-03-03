dcc6018ec37303b1f1ecd94ff431d3de28656821
ch <956718871@qq.com>
2024-03-03 10:21:27 +0800

    effective java笔记

---
+++ leetcode/Day52/SortedStack.java
@@ -0,0 +1,35 @@
+package leetcode.Day52;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class SortedStack {
+    Deque<Integer> stack;
+    Deque<Integer> temp;
+    public SortedStack() {
+        stack=new ArrayDeque<>();
+        temp=new ArrayDeque<>();
+    }
+    
+    public void push(int val) {
+        while (!stack.isEmpty()&&stack.peek()<val){
+            temp.push(stack.pop());
+        }
+        stack.push(val);
+        while (!temp.isEmpty())
+            stack.push(temp.pop());
+    }
+    
+    public void pop() {
+        if(!isEmpty())
+            stack.pop();
+    }
+    
+    public int peek() {
+        return stack.isEmpty()?-1: stack.peek();
+    }
+    
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
