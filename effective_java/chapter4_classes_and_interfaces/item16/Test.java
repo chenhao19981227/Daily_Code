2ef60aad9f523e93b85cb0b0cedcc7f9f53ee814
ch <956718871@qq.com>
2024-03-08 19:13:05 +0800

    effective java item17 使可变性最小化 不可变类测试

---
+++ effective_java/chapter4_classes_and_interfaces/item16/Test.java
@@ -0,0 +1,9 @@
+package effective_java.chapter4_classes_and_interfaces.item16;
+
+public class Test {
+    public static void main(String[] args) {
+        Complex complex = new Complex(1, 2);
+        Complex plus = complex.plus(new Complex(2, 3));
+        System.out.println(plus==complex);
+    }
+}
