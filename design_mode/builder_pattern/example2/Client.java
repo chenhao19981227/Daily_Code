914ffdcda61a71151bff49cc55c0e7f536898962
ch <956718871@qq.com>
2024-03-11 10:52:13 +0800

    设计模式：建造者模式 测试

---
+++ design_mode/builder_pattern/example2/Client.java
@@ -0,0 +1,16 @@
+package design_mode.builder_pattern.example2;
+
+public class Client {
+    public static void main(String[] args) {
+        //构建Phone对象
+        Phone phone = new Phone("intel","三星屏幕","金士顿","华硕");
+        System.out.println(phone);
+        Phone2 phone2 = new Phone2.Builder()
+                .cpu("intel")
+                .mainboard("华硕")
+                .memory("金士顿")
+                .screen("三星")
+                .build();
+        System.out.println(phone);
+    }
+}
