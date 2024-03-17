eae260be91eafb305911db7f0294239c7975964c
ch <956718871@qq.com>
2024-03-17 21:23:58 +0800

    设计模式笔记：适配器模式 对象适配器模式 测试

--- design_mode/decorator_pattern/Client.java
+++ design_mode/decorator_pattern/Client.java
@@ -4,5 +4,7 @@ public class Client {
     public static void main(String[] args) {
         Computer computer=new Computer();
         System.out.println(computer.readSD(new SDAdapterTF()));
+
+        System.out.println(computer.readSD(new SDAdapterTF2(new TFCardImpl())));
     }
 }
