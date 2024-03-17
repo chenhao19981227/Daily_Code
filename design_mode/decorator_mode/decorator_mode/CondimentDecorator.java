209d3bc674a79538b9454bb44fdc44f399e00513
ch <956718871@qq.com>
2024-03-17 21:24:15 +0800

    ""

--- design_mode/decorator_mode/decorator_mode/CondimentDecorator.java
+++
@@ -1,6 +0,0 @@
-package design_mode.decorator_mode.decorator_mode;
-
-public abstract class CondimentDecorator extends Beverage{
-    // 继承Beverage是为了装饰者也能被装饰，即可以添加多种调料
-    public abstract String getDescription();
-}
