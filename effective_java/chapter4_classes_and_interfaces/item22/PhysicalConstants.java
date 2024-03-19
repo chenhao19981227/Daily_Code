1d7db0d8e43d86256d1fa1b79fde4f7ff3aa8607
ch <956718871@qq.com>
2024-03-19 12:13:48 +0800

    effective java item22：接口只用于定义类型 常量工具类

---
+++ effective_java/chapter4_classes_and_interfaces/item22/PhysicalConstants.java
@@ -0,0 +1,13 @@
+package effective_java.chapter4_classes_and_interfaces.item22;
+
+public class PhysicalConstants {
+    private PhysicalConstants(){}
+    //阿伏伽德罗数
+    public static final double AVOGADROS_NUMBER = 6.02214199e23;
+
+    //玻尔兹曼常数
+    public static final double BOLRZMANN_CONSTANT = 1.3806503E-23;
+
+    //电子质量
+    public static final double ELECTRON_MASS = 9.10938188E-31;
+}
