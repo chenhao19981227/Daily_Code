b1066edb5f5512016b38f818387ee942b41431b7
ch <956718871@qq.com>
2024-03-19 12:14:00 +0800

    effective java item22：接口只用于定义类型 静态导入

---
+++ effective_java/chapter4_classes_and_interfaces/item22/Test.java
@@ -0,0 +1,9 @@
+package effective_java.chapter4_classes_and_interfaces.item22;
+
+import static effective_java.chapter4_classes_and_interfaces.item22.PhysicalConstants.AVOGADROS_NUMBER;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(AVOGADROS_NUMBER * 10);
+    }
+}
