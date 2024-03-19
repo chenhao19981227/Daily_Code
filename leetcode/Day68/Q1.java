d4fab44ece0891c21b8996f452feacf557ba9e06
ch <956718871@qq.com>
2024-03-19 10:46:07 +0800

    面试题10.09 排序矩阵查找 矩阵

---
+++ leetcode/Day68/Q1.java
@@ -0,0 +1,22 @@
+package leetcode.Day68;
+
+public class Q1 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                --y;
+            } else {
+                ++x;
+            }
+        }
+        return false;
+    }
+}
