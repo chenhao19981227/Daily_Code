a2a117fc6f70d2689859e3dfe69ee8a2208a0a66
ch <956718871@qq.com>
2024-03-22 12:21:07 +0800

    面试题16.05 阶乘尾数 数学

---
+++ leetcode/Day71/Q2.java
@@ -0,0 +1,12 @@
+package leetcode.Day71;
+
+public class Q2 {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        while (n != 0) {
+            n /= 5;
+            ans += n;
+        }
+        return ans;
+    }
+}
