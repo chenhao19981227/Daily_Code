0956f436496c3bd6d65ff760a377645bac44e89b
ch <956718871@qq.com>
2024-03-22 19:46:44 +0800

    设计模式：外观模式 示例

---
+++ design_mode/facade_pattern/TV.java
@@ -0,0 +1,11 @@
+package design_mode.facade_pattern;
+
+public class TV {
+    public void on() {
+        System.out.println("打开了电视....");
+    }
+
+    public void off() {
+        System.out.println("关闭了电视....");
+    }
+}
