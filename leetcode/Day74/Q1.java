f099391c26bac88a3271305c1481f273f16d01fe
ch <956718871@qq.com>
2024-03-25 13:38:51 +0800

    面试题16.11 跳水版 数学

---
+++ leetcode/Day74/Q1.java
@@ -0,0 +1,13 @@
+package leetcode.Day74;
+
+public class Q1 {
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if(k==0)return new int[]{0};
+        if(shorter==longer) return new int[]{shorter*k};
+        int[] res=new int[k+1];
+        for (int i = 0; i <=k; i++) {
+            res[i]=shorter*(k-i)+longer*i;
+        }
+        return res;
+    }
+}
