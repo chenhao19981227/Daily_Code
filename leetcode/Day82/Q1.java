31fd91f4cc91bce6c2bcdd645478b2f8817b57f2
ch <956718871@qq.com>
2024-04-02 20:23:05 +0800

    面试题05.02 二进制数转字符串 位运算 字符串

---
+++ leetcode/Day82/Q1.java
@@ -0,0 +1,14 @@
+package leetcode.Day82;
+
+public class Q1 {
+    public String printBin(double num) {
+        StringBuilder sb = new StringBuilder("0.");
+        while (sb.length() <= 32 && num != 0) {
+            num *= 2;
+            int digit = (int) num;
+            sb.append(digit);
+            num -= digit;
+        }
+        return sb.length() <= 32 ? sb.toString() : "ERROR";
+    }
+}
