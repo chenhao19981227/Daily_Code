d2a027511f5aace7b120cf336d29094bb15ca234
ch <956718871@qq.com>
2024-04-06 18:19:41 +0800

    effective java：静态成员类优于非静态成员类 静态成员类测试

---
+++ effective_java/chapter4_classes_and_interfaces/item24/Test.java
@@ -0,0 +1,8 @@
+package effective_java.chapter4_classes_and_interfaces.item24;
+
+public class Test {
+    public static void main (String[] args) {
+        Outer.Inner inner = new Outer.Inner () ;
+        inner.show();
+    }
+}
