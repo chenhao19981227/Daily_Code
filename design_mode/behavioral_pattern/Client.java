648e032bacc8783cd9d7a60f426166ec00568678
ch <956718871@qq.com>
2024-04-07 18:12:17 +0800

    设计模式：行为模式 错误方式 测试

---
+++ design_mode/behavioral_pattern/Client.java
@@ -0,0 +1,12 @@
+package design_mode.behavioral_pattern;
+
+public class Client {
+    public static void main(String[] args) {
+        Lift lift = new Lift();
+        lift.setState(ILift.STOPPING_STATE);//电梯是停止的
+        lift.open();//开门
+        lift.close();//关门
+        lift.run();//运行
+        lift.stop();//停止
+    }
+}
