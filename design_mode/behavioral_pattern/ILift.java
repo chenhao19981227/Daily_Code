60c3d2ed85f085b25de48efa608bb493a05c41e8
ch <956718871@qq.com>
2024-04-07 18:12:10 +0800

    设计模式：行为模式 错误方式 示例

---
+++ design_mode/behavioral_pattern/ILift.java
@@ -0,0 +1,22 @@
+package design_mode.behavioral_pattern;
+
+public interface ILift {
+    //电梯的4个状态
+    //开门状态
+    public final static int OPENING_STATE = 1;
+    //关门状态
+    public final static int CLOSING_STATE = 2;
+    //运行状态
+    public final static int RUNNING_STATE = 3;
+    //停止状态
+    public final static int STOPPING_STATE = 4;
+
+    //设置电梯的状态
+    public void setState(int state);
+
+    //电梯的动作
+    public void open();
+    public void close();
+    public void run();
+    public void stop();
+}
