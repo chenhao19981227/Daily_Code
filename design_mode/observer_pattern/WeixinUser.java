351cea1fb3cd06b59efae700ceb42efc9abff0d8
ch <956718871@qq.com>
2024-04-09 19:20:54 +0800

    设计模式：观察者模式 具体观察者

---
+++ design_mode/observer_pattern/WeixinUser.java
@@ -0,0 +1,14 @@
+package design_mode.observer_pattern;
+
+public class WeixinUser implements Observer {
+    // 微信用户名
+    private String name;
+
+    public WeixinUser(String name) {
+        this.name = name;
+    }
+    @Override
+    public void update(String message) {
+        System.out.println(name + "-" + message);
+    }
+}
