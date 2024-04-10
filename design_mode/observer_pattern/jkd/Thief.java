7420f65a1df2eefbeb2ec37786ba156a49bad1c8
ch <956718871@qq.com>
2024-04-10 22:08:21 +0800

    设计模式：观察者模式 jdk实现 被观察者

---
+++ design_mode/observer_pattern/jkd/Thief.java
@@ -0,0 +1,26 @@
+package design_mode.observer_pattern.jkd;
+
+import java.util.Observable;
+
+public class Thief extends Observable {
+
+    private String name;
+
+    public Thief(String name) {
+        this.name = name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void steal() {
+        System.out.println("小偷：我偷东西了，有没有人来抓我！！！");
+        super.setChanged(); //changed  = true
+        super.notifyObservers();
+    }
+}
