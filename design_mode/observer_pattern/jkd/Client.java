d831b30dfcc80ca3aba4b99ca9f6f203e7f40021
ch <956718871@qq.com>
2024-04-10 22:08:36 +0800

    设计模式：观察者模式 jdk实现 测试

---
+++ design_mode/observer_pattern/jkd/Client.java
@@ -0,0 +1,14 @@
+package design_mode.observer_pattern.jkd;
+
+public class Client {
+    public static void main(String[] args) {
+        //创建小偷对象
+        Thief t = new Thief("隔壁老王");
+        //创建警察对象
+        Policemen p = new Policemen("小李");
+        //让警察盯着小偷
+        t.addObserver(p);
+        //小偷偷东西
+        t.steal();
+    }
+}
