1ed5c7f27dd4b511f1a962472d77692f0d14e5cd
ch <956718871@qq.com>
2024-04-11 21:19:29 +0800

    设计模式：中介者模式 承租人

---
+++ design_mode/mediator_pattern/Tenant.java
@@ -0,0 +1,17 @@
+package design_mode.mediator_pattern;
+
+public class Tenant extends Person {
+    public Tenant(String name, Mediator mediator) {
+        super(name, mediator);
+    }
+
+    //与中介者联系
+    public void constact(String message){
+        mediator.constact(message, this);
+    }
+
+    //获取信息
+    public void getMessage(String message){
+        System.out.println("租房者" + name +"获取到的信息：" + message);
+    }
+}
