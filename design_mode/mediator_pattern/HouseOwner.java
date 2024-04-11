40185bf107b292deba17e1632e65890497100b04
ch <956718871@qq.com>
2024-04-11 21:19:06 +0800

    设计模式：中介者模式 房屋拥有者

---
+++ design_mode/mediator_pattern/HouseOwner.java
@@ -0,0 +1,18 @@
+package design_mode.mediator_pattern;
+
+public class HouseOwner extends Person {
+
+    public HouseOwner(String name, Mediator mediator) {
+        super(name, mediator);
+    }
+
+    //与中介者联系
+    public void constact(String message){
+        mediator.constact(message, this);
+    }
+
+    //获取信息
+    public void getMessage(String message){
+        System.out.println("房主" + name +"获取到的信息：" + message);
+    }
+}
