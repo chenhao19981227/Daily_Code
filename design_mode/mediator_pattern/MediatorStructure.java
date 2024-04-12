e8dca110cc2a59fb8b3bc97fd9519f6b49d4ca2b
ch <956718871@qq.com>
2024-04-12 23:41:16 +0800

    设计模式：中介者模式 中介机构类

---
+++ design_mode/mediator_pattern/MediatorStructure.java
@@ -0,0 +1,31 @@
+package design_mode.mediator_pattern;
+
+public class MediatorStructure extends Mediator {
+    //首先中介结构必须知道所有房主和租房者的信息
+    private HouseOwner houseOwner;
+    private Tenant tenant;
+
+    public HouseOwner getHouseOwner() {
+        return houseOwner;
+    }
+
+    public void setHouseOwner(HouseOwner houseOwner) {
+        this.houseOwner = houseOwner;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void setTenant(Tenant tenant) {
+        this.tenant = tenant;
+    }
+
+    public void constact(String message, Person person) {
+        if (person == houseOwner) {          //如果是房主，则租房者获得信息
+            tenant.getMessage(message);
+        } else {       //反正则是房主获得信息
+            houseOwner.getMessage(message);
+        }
+    }
+}
