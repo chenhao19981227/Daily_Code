43cea1041a3bb2a49fa4d48c62f6309592a5d98c
ch <956718871@qq.com>
2024-04-15 21:41:09 +0800

    设计模式：访问者模式 测试

---
+++ design_mode/visitor_pattern/Client.java
@@ -0,0 +1,15 @@
+package design_mode.visitor_pattern;
+
+public class Client {
+    public static void main(String[] args) {
+        Home home = new Home();
+        home.add(new Dog());
+        home.add(new Cat());
+
+        Owner owner = new Owner();
+        home.action(owner);
+
+        Someone someone = new Someone();
+        home.action(someone);
+    }
+}
