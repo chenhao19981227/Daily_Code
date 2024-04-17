77347f7970bfe085c555f7c6d9162694f28f76c9
ch <956718871@qq.com>
2024-04-17 20:41:39 +0800

    设计模式：备忘录模式 角色类

---
+++ design_mode/memento_pattern/GameRole.java
@@ -0,0 +1,42 @@
+package design_mode.memento_pattern;
+
+import lombok.Data;
+
+@Data
+public class GameRole {
+    private int vit; //生命力
+    private int atk; //攻击力
+    private int def; //防御力
+
+    //初始化状态
+    public void initState() {
+        this.vit = 100;
+        this.atk = 100;
+        this.def = 100;
+    }
+
+    //战斗
+    public void fight() {
+        this.vit = 0;
+        this.atk = 0;
+        this.def = 0;
+    }
+
+    //保存角色状态
+    public RoleStateMemento saveState() {
+        return new RoleStateMemento(vit, atk, def);
+    }
+
+    //回复角色状态
+    public void recoverState(RoleStateMemento roleStateMemento) {
+        this.vit = roleStateMemento.getVit();
+        this.atk = roleStateMemento.getAtk();
+        this.def = roleStateMemento.getDef();
+    }
+
+    public void stateDisplay() {
+        System.out.println("角色生命力：" + vit);
+        System.out.println("角色攻击力：" + atk);
+        System.out.println("角色防御力：" + def);
+    }
+}
