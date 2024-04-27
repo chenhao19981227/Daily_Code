0e5a971444ee7f4813570f22617bc62567918679
ch <956718871@qq.com>
2024-04-27 12:27:35 +0800

    effective java：限制源文件为单个顶级类 示例

---
+++ effective_java/chapter4_classes_and_interfaces/item25/Main.java
@@ -0,0 +1,9 @@
+package effective_java.chapter4_classes_and_interfaces.item25;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+
+}
---
+++ effective_java/chapter4_classes_and_interfaces/item25/Utensil.java
@@ -0,0 +1,10 @@
+package effective_java.chapter4_classes_and_interfaces.item25;
+
+class Utensil {
+    static final String NAME = "pan";
+}
+
+class Dessert {
+    static final String NAME = "cake";
+
+}
