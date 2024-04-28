fad3f4c0b277749c7264ebb6c287849bba62e133
ch <956718871@qq.com>
2024-04-28 10:53:43 +0800

    1211.查询结果的质量和占比 sql

---
+++ leetcode/Day108/Q2.sql
@@ -0,0 +1,5 @@
+select query_name,round(sum(rating/position)/count(query_name),2) as quality,
+       round(sum(if(rating<3,1,0))/count(query_name)*100,2) as poor_query_percentage
+from Queries
+group by query_name
+having query_name is not null
