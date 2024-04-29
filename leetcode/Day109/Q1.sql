344a4fc906a5a607c6a75d166ec99751b88cac63
ch <956718871@qq.com>
2024-04-29 22:00:46 +0800

    1193.每月交易Ⅰ sql

---
+++ leetcode/Day109/Q1.sql
@@ -0,0 +1,5 @@
+select SUBSTR(trans_date ,1,7) as month,country,count(id) as trans_count,
+        sum(if(state='approved',1,0)) as approved_count,sum(amount) as trans_total_amount,
+        sum(if(state='approved',amount,0)) as approved_total_amount
+from Transactions
+group by country, month
