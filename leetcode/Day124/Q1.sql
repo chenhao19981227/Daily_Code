44f59106450078ade1fceef5f8c58596c2d9765b
ch <956718871@qq.com>
2024-05-14 20:37:42 +0800

    1907.按分类统计薪水 sql

---
+++ leetcode/Day124/Q1.sql
@@ -0,0 +1,8 @@
+select 'Low Salary' as category, sum(if(income<20000,1,0)) as accounts_count
+from Accounts
+union
+select 'Average Salary' as category, sum(if(income<=50000 and income>=20000,1,0)) as accounts_count
+from Accounts
+union
+select 'High Salary' as category, sum(if(income>50000,1,0)) as accounts_count
+from Accounts
