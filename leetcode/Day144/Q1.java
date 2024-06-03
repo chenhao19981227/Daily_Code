cf56275d2a6351cde33335123d2d814b94e73af4
ch <956718871@qq.com>
2024-06-03 22:00:46 +0800

    1103.分糖果Ⅱ 暴力解

---
+++ leetcode/Day144/Q1.java
@@ -0,0 +1,14 @@
+package leetcode.Day144;
+
+public class Q1 {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ans = new int[num_people];
+        int i = 0;
+        while (candies != 0) {
+            ans[i % num_people] += Math.min(candies, i + 1);
+            candies -= Math.min(candies, i + 1);
+            i += 1;
+        }
+        return ans;
+    }
+}
