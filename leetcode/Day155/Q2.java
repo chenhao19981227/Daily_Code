8d0d6fb9897f3470e511508d8cff37fe3511aa09
ch <956718871@qq.com>
2024-06-15 23:51:19 +0800

    165.比较版本号 双指针 精简解法

---
+++ leetcode/Day155/Q2.java
@@ -0,0 +1,16 @@
+package leetcode.Day155;
+
+public class Q2 {
+    public int compareVersion(String version1, String version2) {
+        String[] a1 = version1.split("\\.");
+        String[] a2 = version2.split("\\.");
+
+        for(int n = 0; n < Math.max(a1.length, a2.length); n++){
+            int i = (n < a1.length ? Integer.valueOf(a1[n]) : 0);
+            int j = (n < a2.length ? Integer.valueOf(a2[n]) : 0);
+            if(i < j) return -1;
+            else if(i > j) return 1;
+        }
+        return 0;
+    }
+}
